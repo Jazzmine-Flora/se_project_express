4f6fcb081298c499a6aa1b6ba703698c4081ddeb
Jazzmine-Flora <taliba.latif@gmail.com>
2025-01-10 23:23:01 -0500

    update items

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -21,4 +21,17 @@ const getItems = (req, res) => {
     });
 };
 
-module.exports = { createItem, getItems };
+const updateItem = (req, res) => {
+  const { itemId } = req.params;
+  const { imageURL } = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } })
+    .orFail()
+    .then((item) => res.status(200).send({ data: item }))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send({ message: "Update items error", err });
+    });
+};
+
+module.exports = { createItem, getItems, updateItem };
--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
 
-const { createItem, getItems } = require("../controllers/clothingItem");
+const {
+  createItem,
+  getItems,
+  updateItem,
+} = require("../controllers/clothingItem");
 
 router.post("/", createItem);
 
 router.get("/", getItems);
+
+router.put("/:itemId", updateItem);
 
 module.exports = router;
