aaf748313d2a11ee5ead209def3b1f76dbd48468
Jazzmine-Flora <taliba.latif@gmail.com>
2025-01-15 21:29:42 -0500

    fixed eslint errors

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItem = require("./clothingItems");
-const errors = require("../utils/errors");
+const { NOT_FOUND } = require("../utils/errors");
 
 router.use("/items", clothingItem);
 router.use("/users", userRouter);
