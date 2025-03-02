97d0b889affc16c2479478d7738ae8ac25d1a6a8
Jazzmine-Flora <taliba.latif@gmail.com>
2025-03-02 10:28:35 -0500

    moved createuser

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItem = require("./clothingItems");
 const { NOT_FOUND } = require("../utils/errors");
+const { createUser } = require("../controllers/users");
 
 router.use("/items", clothingItem);
 router.use("/users", userRouter);
+router.post("/", createUser);
 
 router.use((req, res) =>
   res.status(NOT_FOUND).send({ message: "Router Not found" })
