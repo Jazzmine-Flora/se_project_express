0811eee5f1104e7ef0fb648fdc298188ff1d8dda
Jazzmine-Flora <taliba.latif@gmail.com>
2025-03-04 16:46:47 -0500

    task 2

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItem = require("./clothingItems");
 const { NOT_FOUND } = require("../utils/errors");
-// const { createUser } = require("../controllers/users");
+const { createUser } = require("../controllers/users");
 
 router.use("/items", clothingItem);
 router.use("/users", userRouter);
-// router.post("/", createUser);
+router.post("/signup", createUser);
 
 router.use((req, res) =>
   res.status(NOT_FOUND).send({ message: "Router Not found" })
